17b42623f3c87f5bf51a34308b14210f706ff0db
Harsh <harshkind56@gmail.com>
2023-12-28 17:42:08 +0530

    Episodes-1,2,3

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
-const heading = React.createElement("h1", {id: "heading"}, "hello world from react");
-console.log(heading)
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// using react - const heading = React.createElement("h1", {id:"heading"}, "namaste");
+
+//using react - root.render(heading);
+// using JSX
+const jsxHeading = (
+  <h1 className="head" tabIndex="5">
+    Namaste
+  </h1>
+);
+//React functional component.
+const Title = () => <h1> Hello </h1>;
+const number = 1000;
+const HeadingComponent = () => <h1> {number}Namaste </h1>;
+// const HeadingComponent = () => ( <h1 className = "heading"> Namaste  </h1 );
+
+//writing using react for developer friendly thats we use JSX.
+//how to render it.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-
-const parent = React.createElement("div")
+root.render(jsxHeading);
+//rendering a component
+root.render(<HeadingComponent />);
+//JSX - javascript will not understand it
